b9eac7d21b4f1da0ed77fd7bbfa73ec66321cbe6
Jesse Bartola <jrbartola@gmail.com>
2017-05-13 18:13:03 -0400

    materialize css and scss

---
+++ app.js
@@ -0,0 +1,87 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var mongoose = require('mongoose');
+var expressValidator = require('express-validator');
+var fs = require('fs');
+
+// require('./models/attendee');
+// require('./models/category');
+// require('./models/event');
+// require('./models/organization');
+
+// var attendee = require('./routes/attendee');
+// var category = require('./routes/category');
+// var event = require('./routes/event');
+// var org = require('./routes/organization');
+var index = require('./routes/index');
+
+var app = express();
+
+//var mongoObject = readMongoEnv();
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/inq');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+    var namespace = param.split('.')
+        , root    = namespace.shift()
+        , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+app.use('/', index);
+// app.use('/api/attendee', attendee);
+// app.use('/api/category', category);
+// app.use('/api/event', event);
+// app.use('/api/or g', org);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// function readMongoEnv() {
+//     return JSON.parse(fs.readFileSync('env/mongo.json', 'utf8'));
+// }
+
+module.exports = app;
