5aefe50292e653aa38c44eca173724e7acb24a18
Jesse Bartola <jrbartola@gmail.com>
2017-05-18 13:35:32 -0400

    Index page and login/registration modals

---
+++ public/js/index.js
@@ -0,0 +1,90 @@
+/*
+ * Index.js jQuery handlers
+ */
+
+$(document).ready(function() {
+
+$('.login-btn').on('click', function() {
+
+  swal({
+    title: 'Enter the party!',
+    html:
+      '<span style="display:block;text-align:left;">Login</span>' +
+      '<input id="swal-login" type="email" class="swal2-input">' +
+      '<span style="display:block;text-align:left;">Password</span>' +
+      '<input id="swal-password" type="password" class="swal2-input">',
+    preConfirm: function () {
+      return new Promise(function (resolve) {
+        resolve([
+          $('#swal-login').val(),
+          $('#swal-password').val()
+        ])
+      })
+    },
+    onOpen: function () {
+      $('#swal-login').focus()
+    }
+  }).then(function (result) {
+    swal(JSON.stringify(result))
+  }).catch(swal.noop)
+  
+});
+
+$('.register-btn').on('click', function() {
+
+
+  swal.setDefaults({
+    input: 'text',
+    confirmButtonText: 'Next &rarr;',
+    showCancelButton: true,
+    animation: true
+    //progressSteps: ['Email', 'Setup', '3']
+  })
+
+  var steps = [
+    {
+      title: 'What is your email address?',
+      input: 'email'
+    },
+    {
+      title: 'Set a password',
+      input: 'password'
+    },
+    {
+      title: 'Confirm your password',
+      input: 'password'
+    }
+  ]
+
+  swal.queue(steps).then(function (result) {
+    swal.resetDefaults()
+    if (result[1] != result[2]) {
+      swal({
+        title: 'Uh oh!',
+        text: 'Your passwords do not match. Try again',
+        type: 'error',
+        confirmButtonText: 'Okay',
+        showCancelButton: false
+      })
+    } else {
+
+      // Make call to registration API here...
+      swal({
+        title: 'All done!',
+        text: 'You may now log in with your new credentials',
+        type: 'success',
+        confirmButtonText: 'Nice!',
+        showCancelButton: false
+      })
+    }
+  }, function () {
+    swal.resetDefaults()
+  })
+});
+
+
+
+
+
+// End $(document).ready
+});
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'InQueue' });
+  res.render('index', { title: 'InQueue'});
 });
 
 module.exports = router;
